ee298f5d24e92c13016f5476f535757d378877c6
Guido Pujadas <guidopujadas@gmail.com>
2015-12-06 17:46:43 -0300

    empiezo devuelta

--- Intencion_Inscripcion_Angular/webApp/js/controllers.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var app = angular.module('intencionInscripcionApp')
-app.controller('intencionInscripcionController',function ($scope, IntencionInscripcionService) {
-	
-});
-  
--- Intencion_Inscripcion_Angular/webApp/js/service.js
+++
@@ -1,8 +0,0 @@
-angular.module('intencionInscripcionApp')
-.service('IntencionInscripcionService',function($http){
-	this.obtenerDatos = function(callback){
-		$http.get('/datos').success(function () {
-            alert("PUDE TRAER LOS DATOS")
-        });
-	}
-});
